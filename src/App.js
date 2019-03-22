a8ac87a9a6ea62e9bee0280665d5442c507911fb
PiyushPawar17 <piyushpawar25@gmail.com>
2019-03-23 00:36:00 +0530

    Added Games

--- src/App.js
+++ src/App.js
@@ -1,28 +1,31 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+import games from './games.json';
+
+import './styles.css';
+
+const App = () => (
+	<React.Fragment>
+		<nav className="navbar">
+			<div className="container">
+				<h1 className="navbar__header">Games List</h1>
+				<div className="navbar__info">
+					<p>A list to keep track of my completed games</p>
+					<p>Count: {games.games.length}</p>
+				</div>
+			</div>
+		</nav>
+		<main>
+			<div className="container list">
+				{games.games.map(game => (
+					<div className="card" key={game.name}>
+						<img src={require(`./img/${game.img}`)} alt={game.name} className="card__img" />
+						<div className="card__overlay">{game.name}</div>
+					</div>
+				))}
+			</div>
+		</main>
+	</React.Fragment>
+);
 
 export default App;
