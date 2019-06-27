855317cb17065c96b48d2e4dde4e95a51be99099
PiyushPawar17 <piyushpawar25@gmail.com>
2019-06-27 12:55:43 +0530

    Changed UI

--- src/App.js
+++ src/App.js
@@ -1,29 +1,22 @@
 import React from 'react';
 
-import games from './games.json';
+import { games } from './List';
 
 import './styles.css';
 
 const App = () => (
 	<React.Fragment>
-		<nav className="navbar">
-			<div className="container">
-				<h1 className="navbar__header">Games List</h1>
-				<div className="navbar__info">
-					<p>A list to keep track of my completed games</p>
-					<p>Count: {games.games.length}</p>
+		<header className="header">
+			<h1>Important List</h1>
+			<h2>A list to keep track of my completed games</h2>
+			<p>Count: {games.length}</p>
+		</header>
+		<main className="list">
+			{games.map(game => (
+				<div className="card" key={game}>
+					<p>{game}</p>
 				</div>
-			</div>
-		</nav>
-		<main>
-			<div className="container list">
-				{games.games.map(game => (
-					<div className="card" key={game.name}>
-						<img src={require(`./img/${game.img}`)} alt={game.name} className="card__img" />
-						<div className="card__overlay">{game.name}</div>
-					</div>
-				))}
-			</div>
+			))}
 		</main>
 	</React.Fragment>
 );
